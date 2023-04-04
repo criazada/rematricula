2e99c62536a4eb41695aa835ff5262d5dabbdde8
iagoq <18238046+iagocq@users.noreply.github.com>
2023-04-04 19:19:50 -0300

    reestrutura projeto

--- scrape/tipos.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-
-Oferta = namedtuple('Oferta', ('cod', 'disc', 'nome', 'turma'))
-Disciplina = namedtuple('Disciplina', ('disc', 'nome', 'ofertas'))
-Descricao = namedtuple('Descricao', ('turma', 'curso', 'situacao', 'normal', 'especial', 'horarios'))
-Horario = namedtuple('Horario', ('local', 'maximo', 'ocupacao', 'tipo', 'dia', 'hora'))
-Local = namedtuple('Local', ('desc', 'abbr'))
-Matricula = namedtuple('Matricula', ('oferecidas', 'ocupadas', 'restantes', 'pendentes', 'razao'))
-Hora = namedtuple('Hora', ('inicio', 'fim'))
